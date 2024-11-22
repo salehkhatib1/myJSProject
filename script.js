c42b82ee6f4cc4f62bc83316c10d83f0b3572b66
salehkhatib1 <89403373+salehkhatib1@users.noreply.github.com>
2024-11-22 18:02:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+const now = 2037;
+const salehAge = now - 1996;
+const saraAge = now - 2018;
+
+console.log(salehAge);
+console.log(saraAge);
+
+console.log(salehAge > saraAge);
+console.log(now - 1996 > now - 2018);
+
+let century;
+const birthYear = 1991;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
+
+const inputYear = "1991";
